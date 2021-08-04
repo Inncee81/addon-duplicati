728bb1eec946dc99b86e072676645027d8fce9d0
Elcajon <github@max-venz.de>
2021-08-04 08:51:55 +0200

    Initial Upload

---
+++ duplicati/rootfs/defaults/config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'memcache.local' => '\OC\Memcache\APCu',
+  'default_phone_region' => 'DE',
+  'overwriteprotocol' => 'http',
+  'mail_smtpmode' => 'sendmail',
+  'mail_sendmailmode' => 'smtp',
+  'mail_from_address' => 'nextcloud',
+  'mail_domain' => 'server.local',
+  );
